33d139c6d51e1578c93fbf50e5c54c712e0900e3
TheSerch07 <jejog50@gmail.com>
2023-03-06 18:28:59 -0500

    avances

---
+++ src/components/CreateLogo.jsx
@@ -0,0 +1,13 @@
+import styles from "../styles/CreateLogo.module.css"
+
+function CreateLogo() {
+    return (
+        <div className={styles.mainCreateLogo}>
+            <div className=""> 
+                <img src=""/>
+            </div>
+        </div>
+    )
+}
+
+export default CreateLogo
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,18 +7,6 @@ function Header() {
 	return (
         <header className={styles.mainHeader}>
 			<img src={logo} alt="LogoWawo" />
-		{/* <a id="logo-header" href="#">
-			<span class="site-name">FranciscoAMK</span>
-			<span class="site-desc">Diseño web / WordPress / Tutoriales</span>
-		</a>
-
-		<nav>
-			<ul>
-				<li><a href="#">Inicio</a></li>
-				<li><a href="#">Acerca de</a></li>
-				<li><a href="#">Contacto</a></li>
-			</ul>
-		</nav> */}
 	    </header>
     )
 }
