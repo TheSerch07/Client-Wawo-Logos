a59feca19e7226ef598307447055296f45c060f7
TheSerch07 <jejog50@gmail.com>
2023-03-06 19:52:51 -0500

    body 2

--- src/components/CreateLogo.jsx
+++ src/components/CreateLogo.jsx
@@ -1,10 +1,16 @@
 import styles from "../styles/CreateLogo.module.css"
+import ilustracion from "../assets/Ilustracion1.svg"
 
 function CreateLogo() {
     return (
         <div className={styles.mainCreateLogo}>
-            <div className=""> 
-                <img src=""/>
+            <div className="a"> 
+                <img src={ilustracion} alt="Ilustración1"/>
+            </div>
+            <div>
+                <h1>Obtén el logo perfecto para tu empresa</h1>
+                <p>Ahorra tiempo y dinero con nuestra plataforma impulsada por IA</p>
+                <input type="text" />
             </div>
         </div>
     )
