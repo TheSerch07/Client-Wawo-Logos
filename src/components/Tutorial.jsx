467d2332a6ee8c7c5e71ab0201f375f3a3ce47ca
TheSerch07 <jejog50@gmail.com>
2023-03-09 15:58:48 -0500

    diseño

--- src/components/Tutorial.jsx
+++ src/components/Tutorial.jsx
@@ -1,9 +1,22 @@
 import styles from "../styles/Tutorial.module.css"
+import PasoUno from "../assets/Paso1.svg"
 
 function Tutorial() {
     return (
         <div className={styles.mainTutorial}>
             <h1 className={styles.title}>¿Cómo funciona?</h1>
+            <div className={styles.childTutorial}>
+                <div className={styles.mainText}>
+                    <div className={styles.contentH2}>
+                        <h2>1.</h2>
+                    </div>
+                    <h2>Ingresa la información<br></br>de tu empresa</h2>
+                    <p className={styles.p}>Cuéntamos qué haces, cómo se llama<br></br>tu empresa, si tienes un eslogan y las<br></br>palabras que mejor la describan.</p>
+                </div>
+                <div>
+                    <img src={PasoUno} alt="Paso1"/>
+                </div>
+            </div>
         </div>
     )
 }
