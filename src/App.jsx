82de5336beab919538eae5ff35f6e04dff14e90f
TheSerch07 <jejog50@gmail.com>
2023-03-08 20:12:30 -0500

    Create Logo End

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 import Header from './components/Header'
 import CreateLogo from './components/CreateLogo'
+import Tutorial from './components/Tutorial'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
     <div className="App">
       <Header />
       <CreateLogo />
+      <Tutorial />
     </div>
   )
 }
---
+++ src/components/Tutorial.jsx
@@ -0,0 +1,9 @@
+function Tutorial() {
+    return (
+        <div>
+            <h1>¿Cómo funciona?</h1>
+        </div>
+    )
+}
+
+export default Tutorial
