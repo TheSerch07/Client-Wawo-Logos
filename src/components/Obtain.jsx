a6b1eb7d6d9c1cc1b29e5b0a6f38c7871782f1c0
TheSerch07 <jejog50@gmail.com>
2023-03-16 16:19:01 -0500

    section obtains

--- src/components/Obtain.jsx
+++ src/components/Obtain.jsx
@@ -1,11 +1,17 @@
+import styles from "../styles/Obtain.module.css"
+import OpcionesLogo from "../assets/OpcionesLogo.svg"
+
 function Obtain({ price, img, title, paragrah }) {
     return (
-        <div>
-            <div>
-                aaa
+        <div className={styles.mainObtain}>
+            <div className={styles.divPrice}>
+                Gratis
             </div>
-            <h2>Ejemplo</h2>
-            <p>Lorem ipsum dolor </p>
+            <img className={styles.img} src={OpcionesLogo} alt="OpcionesLogo"/>
+            <div>
+                <h2>Aqui va lo que va</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur a neque vel nisl varius tincidunt.</p>
+            </div>
         </div>
     )
 }
