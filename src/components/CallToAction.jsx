94645cfb0032a7067b74f76db275785dc6dcc783
TheSerch07 <jejog50@gmail.com>
2023-03-16 19:34:06 -0500

    CallToAction Ready

--- src/components/CallToAction.jsx
+++ src/components/CallToAction.jsx
@@ -6,9 +6,9 @@ function CallToAction() {
             <div className={styles.divTitle}>
                 <h1 className={styles.title}>¿Qué esperas para tener el logo perfecto para tu marca?</h1>
             </div>   
-            <div>
-                <input type="text" placeholder="Introduce el nombre de tu empresa"/>
-                <button>Quiero mi logo</button>
+            <div className={styles.divButton}>
+                <input className={styles.input} type="text" placeholder="Introduce el nombre de tu empresa"/>
+                <button className={styles.button}>Quiero mi logo</button>
             </div>
         </div>
     )
