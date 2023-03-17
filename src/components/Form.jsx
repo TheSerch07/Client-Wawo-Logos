946cceb940dce63ef40660bb309d417fa85dbce7
TheSerch07 <jejog50@gmail.com>
2023-03-17 17:09:40 -0500

    input

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,13 @@
+import { useState } from ("react")
+
+
 function Form() {
+    
+    const [form, useForm] = useState()
+    
     return (
         <form>
-            
+            <input type="text" />
         </form>
     )
 }
