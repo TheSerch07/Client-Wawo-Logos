7dab1a2c1b04d885b229e92f89d22fe2f9ec53ac
TheSerch07 <jejog50@gmail.com>
2023-03-22 20:48:50 -0500

    ay vamos

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,12 @@
+import { BiArrowBack } from "react-icons/bi";
+import styles from "../styles/Form.module.css";
+
 function Form() {
+
+    console.log(BiArrowBack)
     return(
-        <div>
-            <button>Atrás</button>
+        <div className={styles.mainForm}>
+            <button><BiArrowBack /> Atrás</button>
             <h1>Cuéntanos más</h1>
             <p>Queremos conocer tu empresa. Cuéntanos de qué se trata, cual es su razón de ser y qué productos o servicios ofrece.</p>
             <input type="text" placeholder="Cuéntanos más de tu empresa"/>
--- src/components/Forms.jsx
+++ src/components/Forms.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Form from "./Form";
 import Header from "./Header";
+import styles from "../styles/Forms.module.css"
 
 function Forms() {
     
@@ -14,7 +15,7 @@ function Forms() {
     });
     
     return (
-        <div>
+        <div className={styles.mainForms}>
             <Header useStyle={false}/>
             <Form/>
         </div>
