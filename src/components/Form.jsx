8096d70b3ef4da140d5dfe62e304efc367cc14bf
TheSerch07 <jejog50@gmail.com>
2023-03-24 16:06:03 -0500

    modulos

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,18 +1,8 @@
 import { BiArrowBack } from "react-icons/bi";
 import { BiRightArrowAlt } from "react-icons/bi";
 import styles from "../styles/Form.module.css";
-import { useNavigate } from "react-router-dom";
 
 function Form({ componenteAnterior }){
-
-    // console.log(clickback)
-    const navigate = useNavigate()
-
-    // function componenteAnterior(component) {
-    //     if (component === 1) {
-    //         navigate("/")
-    //     }
-    // }
     return(
         <div className={styles.mainForm}>
             <div className={styles.childForm}>
