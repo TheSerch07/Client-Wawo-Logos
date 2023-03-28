4024aae83e7377508132d4e30cdc86490b70fdd4
TheSerch07 <jejog50@gmail.com>
2023-03-27 21:42:25 -0500

    funcionando

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,27 @@
+import { useState } from "react";
 import { BiArrowBack } from "react-icons/bi";
 import { BiRightArrowAlt } from "react-icons/bi";
 import styles from "../styles/Form.module.css";
+import { useDispatch } from "react-redux";
 
-function Form({ componenteAnterior, componenteSiguiente, titulo, parrafo, placeholder }){
+function Form({ action, componenteAnterior, componenteSiguiente, titulo, parrafo, placeholder }){
     
+    const dispatch = useDispatch()
+    // console.log(action("holi"), "llego la action?")
+    const [form, setForm] = useState("")
+
+    function handleInputChange(e) {
+        setForm(e.target.value)
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        dispatch(action(form))
+        // console.log("Pude enviar dos funciones, jeje 🥺")
+        componenteSiguiente()
+
+    }
+
     return(
         <div className={styles.mainForm}>
             <div className={styles.childForm}>
@@ -12,8 +30,8 @@ function Form({ componenteAnterior, componenteSiguiente, titulo, parrafo, placeh
                     <h1 className={styles.h1}>{ titulo }</h1>
                     <p className={styles.p}>{ parrafo }</p>
                 </div>
-                <textarea className={styles.input} placeholder={placeholder}/>
-                <button onClick={componenteSiguiente} className={styles.buttonTwo}>Siguiente paso <BiRightArrowAlt size={30}/> </button>
+                <textarea onChange={handleInputChange} className={styles.input} placeholder={placeholder}/>
+                <button onClick={handleSubmit} className={styles.buttonTwo}>Siguiente paso <BiRightArrowAlt size={30}/> </button>
             </div>
         </div>
     )
--- src/components/Forms.jsx
+++ src/components/Forms.jsx
@@ -1,8 +1,9 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Form from "./Form";
 import Header from "./Header";
 import styles from "../styles/Forms.module.css"
 import { useNavigate } from "react-router-dom";
+import { setDescription, setWords, setSlogan, setStyles, setEmail } from "../features/form/formSlice";
 import { useSelector } from 'react-redux';
 
 function Forms() {
@@ -28,9 +29,9 @@ function Forms() {
         <div>
             <Header useStyle={false}/>
             <div className={styles.mainForms}>
-                {component === 1 && (<Form componenteAnterior={() => componenteAnterior(component)} componenteSiguiente={() => componenteSiguiente(component)} titulo="Cuéntanos más" parrafo="Queremos conocer tu empresa. Cuéntanos de qué se trata, cual es su razón de ser y qué productos o servicios ofrece." placeholder="Cuéntanos más de tu empresa"/>)}
-                {component === 2 && (<Form componenteAnterior={() => componenteAnterior(component)} componenteSiguiente={() => componenteSiguiente(component)} titulo="Escribe 5 palabras clave" parrafo="Escribe las 5 palabras que mejor describan tu empresa." placeholder="Ingresa tus palabras clave"/>)}
-                {component === 3 && (<Form componenteAnterior={() => componenteAnterior(component)} componenteSiguiente={() => componenteSiguiente(component)} titulo="Ingresa un eslogan" parrafo="Escribe una corta y llamativa frase para incorporar en tu logo." placeholder="Introduce tu eslogan (Opcional)"/>)}
+                {component === 1 && (<Form action={setDescription} componenteAnterior={() => componenteAnterior(component)} componenteSiguiente={() => componenteSiguiente(component)} titulo="Cuéntanos más" parrafo="Queremos conocer tu empresa. Cuéntanos de qué se trata, cual es su razón de ser y qué productos o servicios ofrece." placeholder="Cuéntanos más de tu empresa"/>)}
+                {component === 2 && (<Form action={setWords} componenteAnterior={() => componenteAnterior(component)} componenteSiguiente={() => componenteSiguiente(component)} titulo="Escribe 5 palabras clave" parrafo="Escribe las 5 palabras que mejor describan tu empresa." placeholder="Ingresa tus palabras clave"/>)}
+                {component === 3 && (<Form action={setSlogan} componenteAnterior={() => componenteAnterior(component)} componenteSiguiente={() => componenteSiguiente(component)} titulo="Ingresa un eslogan" parrafo="Escribe una corta y llamativa frase para incorporar en tu logo." placeholder="Introduce tu eslogan (Opcional)"/>)}
             </div>
         </div>
     )
